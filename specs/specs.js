98835382f558cd3a8ae8816827854b455e8e45dd
lawton <lawton.a.browning@gmail.com>
2016-01-22 11:32:23 -0800

    make specs works now

--- specs/specs.js
+++ specs/specs.js
@@ -1,19 +1,19 @@
 describe ('counting', function() {
   it ("counts by 1's ultil it reaches the user input value", function() {
-    expect(counting("20")).to.equal("1,2,ping,4,pong,ping,7,8,ping,pong,11,ping,13,14,pingpong,16,17,ping,19,pong");
+    expect(counting("6")).to.eql([1, 2, 'ping', 4, 'pong', 'ping' ]);
   });
 });
 describe ('counting', function() {
   it ("replaces values divisible by 15 with pingpong", function() {
-    expect(counting("15")).to.equal([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 'pong', 11, 'ping', 13, 14, 'pingpong']);
+    expect(counting("15")).to.eql([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 'pong', 11, 'ping', 13, 14, 'pingpong']);
   });
 });
 describe ('counting', function() {
   it ("replaces values divisible by 5 with pong", function() {
-    expect(counting("5")).to.equal([1, 2, 'ping', 4, 'pong' ]);
+    expect(counting("5")).to.eql([1, 2, 'ping', 4, 'pong' ]);
   });
 });describe ('counting', function() {
   it ("replaces values divisible by 3 with ping", function() {
-    expect(counting("3")).to.equal([1, 2, 'ping']);
+    expect(counting("3")).to.eql([1, 2, 'ping']);
   });
 });
