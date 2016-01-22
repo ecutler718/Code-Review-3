f9c77a708b4f8db2ed0660607fde6b186251b4c8
lawton <lawton.a.browning@gmail.com>
2016-01-22 11:10:57 -0800

    updated specs but still being dumb

--- specs/specs.js
+++ specs/specs.js
@@ -5,15 +5,15 @@ describe ('counting', function() {
 });
 describe ('counting', function() {
   it ("replaces values divisible by 15 with pingpong", function() {
-    expect(counting("20")).to.equal("1,2,ping,4,pong,ping,7,8,ping,pong,11,ping,13,14,pingpong,16,17,ping,19,pong");
+    expect(counting("15")).to.equal([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 'pong', 11, 'ping', 13, 14, 'pingpong']);
   });
 });
 describe ('counting', function() {
   it ("replaces values divisible by 5 with pong", function() {
-    expect(counting("20")).to.equal("1,2,ping,4,pong,ping,7,8,ping,pong,11,ping,13,14,pingpong,16,17,ping,19,pong");
+    expect(counting("5")).to.equal([1, 2, 'ping', 4, 'pong' ]);
   });
 });describe ('counting', function() {
   it ("replaces values divisible by 3 with ping", function() {
-    expect(counting("20")).to.equal("1,2,ping,4,pong,ping,7,8,ping,pong,11,ping,13,14,pingpong,16,17,ping,19,pong");
+    expect(counting("3")).to.equal([1, 2, 'ping']);
   });
 });
